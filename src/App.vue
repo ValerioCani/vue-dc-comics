d9e83df5591fa2f5d3874861f432183d2996c340
Valerio <canivalerio93@gmail.com>
2022-08-24 16:23:32 +0200

    creazione componenti pagina

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,26 @@
 <template>
   <div id="app">
     <HeaderSection/>
+    <MainSection/>
+    <FooterSection/>
   </div>
 </template>
 
 <script>
   import HeaderSection from './components/HeaderSection.vue';
+  import MainSection from './components/MainSection.vue';
+  import FooterSection from './components/MainSection.vue';
 
   export default {
     name: 'App',
     components: {
-      HeaderSection
+      HeaderSection,
+      MainSection,
+      FooterSection
     }
   }
 </script>
 
 <style lang="scss">
+
 </style>
